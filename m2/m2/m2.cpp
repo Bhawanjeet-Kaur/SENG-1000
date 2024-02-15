30d858cab390359378a5debaeb4f4f14480ac04c
Bhawanjeet Kaur <bhawanjeetgill@gmail.com>
2024-02-14 20:11:31 -0500

    using the two functions in main function to complete code

--- m2/m2/m2.cpp
+++ m2/m2/m2.cpp
@@ -1,16 +1,55 @@
 /*
 * FILE : m2.cpp
 * PROJECT : SENG1000- major assignment 2
-* PROGRAMMER : Bhawanjeet Kaur Gill
+* PROGRAMMER : 8958829
 * FIRST VERSION : 2024-02-08
 * DESCRIPTION :
 * Write a program that calculates powers using a function that you create.
 */
 #include <stdio.h>
 #include <stdbool.h>
+int getNum();
+int powerCalculation(int, int);
+int kCheckLoop = 0;
 int main(void)
 {
-
+	int base = 1;
+	int exponent = 1;
+	int solution;
+	int userChoice;
+	while (kCheckLoop == 0)
+	{
+		printf("\n\nMenu:\n\t1. Change base\n\t2. Change exponent\n\t3. Display base raised to exponent\n\t4. Exit program\nOption ? ");
+		userChoice = getNum();
+		if (userChoice == 1)
+		{
+			printf("Enter new base: ");
+			base = getNum();
+			//continue;
+		}
+		else if (userChoice == 2)
+		{
+			printf("Enter new exponent: ");
+			exponent = getNum();
+			//continue;
+		}
+		else if (userChoice == 3)
+		{
+			solution = powerCalculation(base, exponent);
+			printf("%d raised to the power of %d is %d\n", base, exponent, solution);
+			//continue;
+		}
+		else if (userChoice == 4)
+		{
+			kCheckLoop = 1;
+			//break;
+		}
+		else
+		{
+			printf("Invalid entry");
+			//continue;
+		}
+	}
 }
 /*powerCalculation() function contains two parameters in it. It is taking in a base number and an exponent for that
 * base number. The function will calculate the power of that base using while loop and an assignment operator.
